86c1d9dab00e67fb1a634e6021c41e3d3be65ad6
brannerchinese <brannerchinese@gmail.com>
2013-05-06 18:13:05 -0400

    revised test.js

--- JS/test.js
+++ JS/test.js
@@ -24,12 +24,13 @@ function getSought() {
     for (var i = 0; i < tupleIndexes.length; i++) {
       var pathHash = tupleIndexes[indexNum++];
       var pathTuple = TupleStorage[String(pathHash)];
-      document.write('<form><input type="text" name="sought" id="searchTerm"/>
-          + '<input type="button" onclick="getSought()" value="Enter search term">'
-          + '</form>'
-          + '<li><a href="'  + urlPrefix + pathTuple[1] + 
-          '" target="_blank">' + pathTuple[2]+ '</a> (<strong>' + 
-        pathTuple[0] + '</strong>: ' + pathTuple[1] + ')</li>');
+      document.write("<form><input type=\"text\" name=\"sought\"" +
+          "id=\"searchTerm\"/>" +
+          "<input type=\"button\" onclick=\"getSought()\" " +
+          "value=\"Enter search term.\"></form>" + 
+          "<li><a href=\""  + urlPrefix + pathTuple[1] + 
+          "\" target=\"_blanki\">" + pathTuple[2]+ "</a> (<strong>" + 
+        pathTuple[0] + "</strong>: " + pathTuple[1] + ")</li>");
     } 
     document.write('</ul>');
   }
