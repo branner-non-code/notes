aa421c77bae9a58f8a06718dbfeb6cf951a55698
David Branner <brannerchinese@gmail.com>
2013-05-28 14:44:18 -0400

    write() => getElementById().innerHTML= and HTML simplified

--- JS/test.js
+++ JS/test.js
@@ -9,7 +9,14 @@ else {
   var urlPrefix = "";
   var whereWeAre = "from the filesystem.";
 }
-document.getElementById("whereRunning").innerHTML="This page was read "+whereWeAre+"<br/><br/>Enter your search term below:";
+document.getElementById("whereRunning").innerHTML="This page was read " + 
+    whereWeAre + "<br/><br/>Enter your search term below:" + 
+    "<form>" + 
+    "<input type='text' id='searchTerm' />" + 
+    "<input type='button' onclick='getSought()' value='Enter search term'>" +
+    "</form>";
+
+
 
 // Set focus.
 document.getElementById("searchTerm").focus();
@@ -32,22 +39,21 @@ function getSought() {
     document.close();
     var tupleIndexes = IndexEntries[sought];
     var indexNum = 0;
-    document.getElementById("whereRunning").innerHTML="Enter your next search term below:";
-    document.write(
+    document.getElementById("whereRunning").innerHTML="Enter your next search term below:" + 
         "<p id=\"whereRunning\"> </p>" +
         "<form><input type=\"text\" id=\"searchTerm\" />" +
         "<input type=\"button\" onclick=\"getSought()\" " +
-          "value=\"Submit search term\"></form>\n <hr>");
+          "value=\"Submit search term\"></form>";
     document.getElementById("searchTerm").focus();
-    document.write("<p>Searching for: "+sought+"</p>");
-    document.write("<ul>");
+    var output = "<p>Results for: "+sought+"</p><ul>"
     for (var i = 0; i < tupleIndexes.length; i++) {
       var pathHash = tupleIndexes[indexNum++];
       var pathTuple = TupleStorage[String(pathHash)];
-      document.write("<li><a href=\""  + urlPrefix + pathTuple[1] + 
-          "\" target=\"_blank\">" + pathTuple[2]+ "</a> (<strong>" + 
-        pathTuple[0] + "</strong>: " + pathTuple[1] + ")</li>");
-    } 
-    document.write('</ul>');
+      output += "<li><a href=\""  + urlPrefix + pathTuple[1] + 
+        "\" target=\"_blank\">" + pathTuple[2]+ "</a> (<strong>" +
+        pathTuple[0] + "</strong>: " + pathTuple[1] + ")</li>";
+    }
+    output += "</ul>";
+    document.getElementById("results").innerHTML=output
   }
 }
