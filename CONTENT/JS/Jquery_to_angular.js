879f38de3afe65846af6f567a49aff6c936e6440
David Branner <brannerchinese@gmail.com>
2014-05-21 12:18:52 -0400

    Create Jquery_to_angular.js

---
+++ CONTENT/JS/Jquery_to_angular.js
@@ -0,0 +1,5 @@
+## Advice about transitioning from jQuery to Angular
+
+http://stackoverflow.com/questions/14994391/how-do-i-think-in-angularjs-if-i-have-a-jquery-background/15012542#15012542
+
+[end]
