b6e4d19f78efd355555702062bc0d78f9d1a01dc
David Branner <brannerchinese@gmail.com>
2014-07-28 16:28:53 -0400

    revise js/test.js

--- js/test.js
+++ js/test.js
@@ -1,56 +1,67 @@
+;(function IIFE(window, document, undefined) {
+  // Variables
+  var currentURL,
+      urlPrefix,
+      whereWeAre,
+      sought,
+      tupleIndexes,
+      indexNum,
+      output,
+      pathHash,
+      pathTuple;
 
-// Different settings if running locally or on web.
-var currentURL = window.location.href;
-if (currentURL.search(/http/) === 0) {
-  // Don't use htmlpreview.github.io here; displays markdown, not HTML.
-  var urlPrefix = "https://github.com/brannerchinese/notes/blob/master/";
-  var whereWeAre = "via HTTP.";
-}
-else {
-  var urlPrefix = "";
-  var whereWeAre = "from the filesystem.";
-}
+  // Main loop.
+  // Different settings if running locally or on web.
+  currentURL = window.location.href;
+  if (currentURL.search(/http/) === 0) {
+    // Don't use htmlpreview.github.io here; displays markdown, not HTML.
+    urlPrefix = "https://github.com/brannerchinese/notes/blob/master/";
+    whereWeAre = "via HTTP.";
+  }
+  else {
+    urlPrefix = "";
+    whereWeAre = "from the filesystem.";
+  }
 
-document.getElementById("whereRunning").innerHTML="This page was read " + 
-    whereWeAre + "<br/><br/>Enter your search term below:" + 
-    "<form>" + 
-    "<input type='text' id='searchTerm' />" + 
-    "<input type='button' onclick='getSought()' value='Enter search term'>" +
-    "</form>";
+  document.getElementById("whereRunning").innerHTML="This page was read " + 
+      whereWeAre + "<br/><br/>Enter your search term below:" + 
+      "<form>" + 
+      "<input type='text' id='searchTerm' />" + 
+      "<input type='button' onclick='getSought()' value='Enter search term'>" +
+      "</form>";
 
+  // Set focus.
+  document.getElementById("searchTerm").focus();
 
-
-// Set focus.
-document.getElementById("searchTerm").focus();
-
-// Errors.
-if (IndexEntries === null) {
-  alert("IndexEntries is null");
-}
-else if (TupleStorage === null) {
-  alert("TupleStorage is null");
-}
+  // Errors.
+  if (IndexEntries === null) {
+    alert("IndexEntries is null");
+  }
+  else if (TupleStorage === null) {
+    alert("TupleStorage is null");
+  }
+})(window, document);
 
 function getSought() {
-  var sought = document.getElementById("searchTerm").value.toLowerCase();
+  sought = document.getElementById("searchTerm").value.toLowerCase();
   if (sought === null || typeof IndexEntries[sought] === "undefined") {
     alert("Nothing found.");
     location.reload()
   }
   else {
     document.close();
-    var tupleIndexes = IndexEntries[sought];
-    var indexNum = 0;
+    tupleIndexes = IndexEntries[sought];
+    indexNum = 0;
     document.getElementById("whereRunning").innerHTML="Enter your next search term below:" + 
         "<p id=\"whereRunning\"> </p>" +
         "<form><input type=\"text\" id=\"searchTerm\" />" +
         "<input type=\"button\" onclick=\"getSought()\" " +
           "value=\"Submit search term\"></form>";
     document.getElementById("searchTerm").focus();
-    var output = "<p>Results for: "+sought+"</p><ul>"
+    output = "<p>Results for: "+sought+"</p><ul>"
     for (var i = 0; i < tupleIndexes.length; i++) {
-      var pathHash = tupleIndexes[indexNum++];
-      var pathTuple = TupleStorage[String(pathHash)];
+      pathHash = tupleIndexes[indexNum++];
+      pathTuple = TupleStorage[String(pathHash)];
       output += "<li><a href=\""  + urlPrefix + pathTuple[1] + 
         "\" target=\"_blank\">" + pathTuple[2]+ "</a> (<strong>" +
         pathTuple[0] + "</strong>: " + pathTuple[1] + ")</li>";
